1ffd171b0f91ba08ff3e50a71a990048cadf3e1f
James Pfleger <james.pfleger@gmail.com>
2023-10-25 12:55:03 -0700

    setting up background for basic map

--- rich.asm
+++ rich.asm
@@ -7,7 +7,7 @@
 	.byte	"NES", $1A	; iNES header identifier
 	.byte	2		    ; 2x 16KB PRG code - lists how much program rom you have
 	.byte   1		    ; 1x  8KB CHR data - lists how much chr rom you have
-	.byte   $01,        ; mapper 0
+	.byte   $01         ; mapper 0
     .byte   $00	        ; vertical mirroring off
     .byte   $00         ; iNES_SRAM
     .byte   $00         ; iNES Mapper?
@@ -17,6 +17,12 @@
 .segment "ZEROPAGE"
     ;; Variables
     ;; Constants
+    WALL_TILE   = $85
+    RIGHT_WALL  = $E5
+    TOP_WALL    = $20
+    BOTTOM_WALL = $E0
+    LEFT_WALL   = $04
+
 .segment "STARTUP"
 .segment "CODE"
 
@@ -26,7 +32,55 @@ vblankwait:
     bpl vblankwait
     rts
 
-reset:
+;; load palette loads the palette data into the PPU palette RAM
+;; addresses are hard coded for now
+loadpalette:
+    LDA $2002   ; clears PPU status reg
+    LDA #$3f    ; upper bit of palette address
+    STA $2006   ; store in ppu address reg
+    LDA #$00    ; lower bit of palette address
+    STA $2006
+
+    LDX #$00    ; make sure X is $00
+loadpaletteloop:
+    LDA palettedata,X
+    STA $2007
+    INX 
+    CPX #$20
+    BNE loadpaletteloop
+    rts
+
+loadnametable:
+    LDA $2002   ; read PPU to reset high/low latch
+    LDA #$20
+    STA $2006   ; write high byte of $2000 address
+    LDA #$00
+    STA $2006   ; write low byte
+    LDX #$00
+:
+    LDA nametabledata,X
+    STA $2007           ; write to PPU
+    INX 
+    CPX #$FF            ; compare X to $80, decimal 128, as copying 128 bytes
+    BNE :-
+    rts
+
+loadattribute:
+    LDA $2002
+    LDA #$23    ; high byte of $23C0
+    STA $2006
+    LDA #$c0    ; low byte
+    STA $2006
+    LDX #$00
+:
+    LDA attributedata,X
+    STA $2007   ; write to PPU
+    INX 
+    CPX #$08    ; copying 8 bytes of data
+    BNE :-
+    rts
+
+RESET:
     sei			; disable IRQs
 	cld			; disable decimal mode
 	ldx	#$40
@@ -39,7 +93,7 @@ reset:
 	stx	$4010		; disable DMC IRQs
 
 	;; first wait for vblank to make sure PPU is ready
-    jsr vblankwait:
+    jsr vblankwait
 
 clear_memory:
 	lda	#$00
@@ -62,6 +116,18 @@ clear_memory:
 	;; second wait for vblank, PPU is ready after this 
     jsr vblankwait
 
+    jsr loadpalette
+
+    jsr loadnametable
+
+    jsr loadattribute
+
+    CLI 
+    LDA #%10010000  ; enable NMI, sprites from pattern table 0, background from pattern table 1
+    STA $2000
+
+    LDA #%10001110  ; background and sprites enable, no clipping on left
+    STA $2001
     ; load initial sprite info
 
 forever:
@@ -69,10 +135,66 @@ forever:
 
 ;;;;;; vblank loop - called every frame ;;;;;
 VBLANK:
-    
+    rti 
+
+palettedata:
+    .byte $22, $29, $1a, $0F, $22, $36, $17, $0F, $22, $30, $21, $0F, $0f, $0f, $0f, $0F  ; background palette data
+    .byte $22, $16, $27, $18, $22, $1A, $30, $27, $22, $16, $30, $27, $0f, $0F, $0f, $0f  ; sprite palette data 
+
+attributedata:
+    .byte %11111111, %00100111, %00100000, %00000000, %00000000, %00000000, %00000000, %00110000
+
+nametabledata:
+    .byte $24,$24,$24,$24,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00   ; row1
+    .byte $00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$24,$24,$24,$24
+
+    .byte $24,$24,$24,$24,$00,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24   ; row 2
+    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$00,$24,$24,$24,$24
+
+    .byte $24,$24,$24,$24,$00,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24   ; row 2
+    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$00,$24,$24,$24,$24
+
+    .byte $24,$24,$24,$24,$00,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24   ; row 2
+    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$00,$24,$24,$24,$24
+
+    .byte $24,$24,$24,$24,$00,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24   ; row 2
+    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$00,$24,$24,$24,$24
+
+    .byte $24,$24,$24,$24,$00,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24   ; row 2
+    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$00,$24,$24,$24,$24
+
+    .byte $24,$24,$24,$24,$00,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24   ; row 2
+    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$00,$24,$24,$24,$24
+
+    .byte $24,$24,$24,$24,$00,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24   ; row 2
+    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$00,$24,$24,$24,$24
+
+    .byte $24,$24,$24,$24,$00,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24   ; row 2
+    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$00,$24,$24,$24,$24
+
+    .byte $24,$24,$24,$24,$00,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24   ; row 2
+    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$00,$24,$24,$24,$24
+
+    .byte $24,$24,$24,$24,$00,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24   ; row 2
+    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$00,$24,$24,$24,$24
+
+    .byte $24,$24,$24,$24,$00,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24   ; row 2
+    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$00,$24,$24,$24,$24
+
+    .byte $24,$24,$24,$24,$00,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24   ; row 2
+    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$00,$24,$24,$24,$24
+
+    .byte $24,$24,$24,$24,$00,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24   ; row 2
+    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$00,$24,$24,$24,$24
+
+    .byte $24,$24,$24,$24,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00   ; row1
+    .byte $00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$24,$24,$24,$24
+
 
 .segment "VECTORS"
     .word VBLANK
     .word RESET
     .word 0
+
 .segment "CHARS"
+    .incbin "mario.chr"
