1abb630db3bc4e5f045b09e761f85640703755f0
James Pfleger <james.pfleger@gmail.com>
2023-10-25 10:20:35 -0700

    header - added comments for what each byte is for

--- rich.asm
+++ rich.asm
@@ -1,14 +1,18 @@
 .segment "HEADER"
 	
+    ; These values tell the emulator what type of cartirdge we are using with what extra features
+    ;   the cartridge has. lets start with 0 features and see if we need to learn about them for this game
+
+    ; 16 byte header
 	.byte	"NES", $1A	; iNES header identifier
 	.byte	2		    ; 2x 16KB PRG code - lists how much program rom you have
 	.byte   1		    ; 1x  8KB CHR data - lists how much chr rom you have
-	.byte   $01, $00	; mapper 0, vertical mirroring
-    ; NEED TO FIGURE OUT WHAT THESE DO below this line
-    .byte   $00
-    .byte   $00
-    .byte   $00
-    .byte   $00, $00, $00, $00, $00
+	.byte   $01,        ; mapper 0
+    .byte   $00	        ; vertical mirroring off
+    .byte   $00         ; iNES_SRAM
+    .byte   $00         ; iNES Mapper?
+    .byte   $00         ; iNES Mapper?
+    .byte   $00, $00, $00, $00, $00  ; padding
 
 .segment "ZEROPAGE"
 .segment "STARTUP"
@@ -45,10 +49,14 @@ clear_memory:
 	inx
 	bne	clear_memory
 
-	;; second wait for vblank, PPU is ready after this
+    ; Other things you can do between vblank waits are set up audio
+    ; or set up other mapper registers.
+
+
+	;; second wait for vblank, PPU is ready after this 
 vblankwait2:
 	bit	$2002
 	bpl	vblankwait2
-    
+
 .segment "VECTORS"
 .segment "CHARS"
