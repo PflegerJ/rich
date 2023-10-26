ce817693f5beaa49a269e204babbd038080b6e2a
James Pfleger <james.pfleger@gmail.com>
2023-10-26 14:45:28 -0700

    set up basic box to practice collisions

--- rich.asm
+++ rich.asm
@@ -4,80 +4,35 @@
     ;   the cartridge has. lets start with 0 features and see if we need to learn about them for this game
 
     ; 16 byte header
-	.byte	"NES", $1A	; iNES header identifier
-	.byte	2		    ; 2x 16KB PRG code - lists how much program rom you have
-	.byte   1		    ; 1x  8KB CHR data - lists how much chr rom you have
+	.byte	"NES" 
+    .byte   $1A	        ; iNES header identifier
+	.byte	$02		    ; 2x 16KB PRG code - lists how much program rom you have
+	.byte   $01		    ; 1x  8KB CHR data - lists how much chr rom you have
 	.byte   $01         ; mapper 0
     .byte   $00	        ; vertical mirroring off
     .byte   $00         ; iNES_SRAM
     .byte   $00         ; iNES Mapper?
     .byte   $00         ; iNES Mapper?
     .byte   $00, $00, $00, $00, $00  ; padding
-
+.segment "STARTUP"
 .segment "ZEROPAGE"
     ;; Variables
+    pointerLo:   .res 1  ; pointer variables are declared in RAM
+    pointerHi:   .res 1  ; low byte first, high byte immediately after
     ;; Constants
-    WALL_TILE   = $85
-    RIGHT_WALL  = $E5
-    TOP_WALL    = $20
-    BOTTOM_WALL = $E0
-    LEFT_WALL   = $04
+   ; WALL_TILE   = $85
+  ;  RIGHT_WALL  = $E5
+   ; TOP_WALL    = $20
+   ; BOTTOM_WALL = $E0
+   ; LEFT_WALL   = $04
 
-.segment "STARTUP"
+
 .segment "CODE"
 
     ;; Subroutines
 vblankwait:
     bit $2002   
     bpl vblankwait
-    rts
-
-;; load palette loads the palette data into the PPU palette RAM
-;; addresses are hard coded for now
-loadpalette:
-    LDA $2002   ; clears PPU status reg
-    LDA #$3f    ; upper bit of palette address
-    STA $2006   ; store in ppu address reg
-    LDA #$00    ; lower bit of palette address
-    STA $2006
-
-    LDX #$00    ; make sure X is $00
-loadpaletteloop:
-    LDA palettedata,X
-    STA $2007
-    INX 
-    CPX #$20
-    BNE loadpaletteloop
-    rts
-
-loadnametable:
-    LDA $2002   ; read PPU to reset high/low latch
-    LDA #$20
-    STA $2006   ; write high byte of $2000 address
-    LDA #$00
-    STA $2006   ; write low byte
-    LDX #$00
-:
-    LDA nametabledata,X
-    STA $2007           ; write to PPU
-    INX 
-    CPX #$FF            ; compare X to $80, decimal 128, as copying 128 bytes
-    BNE :-
-    rts
-
-loadattribute:
-    LDA $2002
-    LDA #$23    ; high byte of $23C0
-    STA $2006
-    LDA #$c0    ; low byte
-    STA $2006
-    LDX #$00
-:
-    LDA attributedata,X
-    STA $2007   ; write to PPU
-    INX 
-    CPX #$08    ; copying 8 bytes of data
-    BNE :-
     rts
 
 RESET:
@@ -95,100 +50,254 @@ RESET:
 	;; first wait for vblank to make sure PPU is ready
     jsr vblankwait
 
-clear_memory:
-	lda	#$00
-	sta	$0000, x
-	sta	$0100, x
-	sta	$0300, x
-	sta	$0400, x
-	sta	$0500, x
-	sta	$0600, x
-	sta	$0700, x
-	lda	#$fe
-	sta	$0200, x	; move all sprites off screen
-	inx
-	bne	clear_memory
+    TXA 
+clearmem:
+    STA $0000,X
+    STA $0100,X
+    STA $0300,X
+    STA $0400,X
+    STA $0500,X
+    STA $0600,X
+    STA $0700,X
+    LDA #$fe
+    STA $0200,X
+    LDA #$00
+    INX 
+    BNE clearmem 
 
-    ; Other things you can do between vblank waits are set up audio
-    ; or set up other mapper registers.
+    JSR vblankwait
 
+    LDA $02     ; high byte for sprite memory
+    STA $4014
+    NOP 
 
-	;; second wait for vblank, PPU is ready after this 
-    jsr vblankwait
+clearnametables:
+    LDA $2002   ; reset PPU status
+    LDA #$20
+    STA $2006
+    LDA #$00
+    STA $2006
+    LDX #$08
+    LDY #$00
+    LDA #$24    ; clear background tile
+:
+    STA $2007
+    DEY 
+    BNE :-
+    DEX 
+    BNE :-
 
-    jsr loadpalette
+loadpalettes:
+    LDA $2002
+    LDA #$3f
+    STA $2006
+    LDA #$00
+    STA $2006
+    LDX #$00
+loadpalettesloop:
+    LDA palette,X   ; load data from adddress (palette + X)
+                        ; 1st time through loop it will load palette+0
+                        ; 2nd time through loop it will load palette+1
+                        ; 3rd time through loop it will load palette+2
+                        ; etc
+    STA $2007
+    INX 
+    CPX #$20
+    BNE loadpalettesloop
 
-    jsr loadnametable
+loadsprites:
+    LDX #$00
+loadspritesloop:
+    LDA sprites,X
+    STA $0200,X
+    INX 
+    CPX #$10
+    BNE loadspritesloop 
 
-    jsr loadattribute
+;;; Using nested loops to load the background efficiently ;;;
+loadbackground:
+    LDA $2002               ; read PPU status to reset the high/low latch
+    LDA #$20
+    STA $2006               ; write high byte of $2000 address
+    LDa #$00
+    STA $2006               ; write low byte of $2000 address
+
+    LDA #<background 
+    STA pointerLo           ; put the low byte of address of background into pointer
+    LDA #>background        ; #> is the same as HIGH() function in NESASM, used to get the high byte
+    STA pointerHi           ; put high byte of address into pointer
+
+    LDX #$00                ; start at pointer + 0
+    LDY #$00
+outsideloop:
+
+insideloop:
+    LDA (pointerLo),Y       ; copy one background byte from address in pointer + Y
+    STA $2007               ; runs 256*4 times
+
+    INY                     ; inside loop counter
+    CPY #$00                
+    BNE insideloop          ; run inside loop 256 times before continuing
+
+    INC pointerHi           ; low byte went from 0 -> 256, so high byte needs to be changed now
+
+    INX                     ; increment outside loop counter
+    CPX #$04                ; needs to happen $04 times, to copy 1KB data
+    BNE outsideloop    
+
+ 
+loadattribute:
+    LDA $2002
+    LDA #$23    ; high byte of $23C0
+    STA $2006
+    LDA #$C0    ; low byte
+    STA $2006
+    LDX #$00
+:
+    LDA attributes,X
+    STA $2007   ; write to PPU
+    INX 
+    CPX #$40    ; copying 8 bytes of data
+    BNE :-
+
 
     CLI 
-    LDA #%10010000  ; enable NMI, sprites from pattern table 0, background from pattern table 1
+    LDA #%10010000  ; enable NMI, sprites from pattern table 0, background from 1
     STA $2000
-
-    LDA #%10001110  ; background and sprites enable, no clipping on left
+    LDA #%00001110  ; background and sprites enable, no left clipping
     STA $2001
-    ; load initial sprite info
 
+    LDA #$00    ; reset scroll address 
+    STA $2005
+    sta $2005
+    
 forever:
     jmp forever
 
 ;;;;;; vblank loop - called every frame ;;;;;
 VBLANK:
-    rti 
 
-palettedata:
+
+    RTI
+
+palette:
     .byte $22, $29, $1a, $0F, $22, $36, $17, $0F, $22, $30, $21, $0F, $0f, $0f, $0f, $0F  ; background palette data
     .byte $22, $16, $27, $18, $22, $1A, $30, $27, $22, $16, $30, $27, $0f, $0F, $0f, $0f  ; sprite palette data 
 
-attributedata:
-    .byte %11111111, %00100111, %00100000, %00000000, %00000000, %00000000, %00000000, %00110000
+attributes:  ;8 x 8 = 64 bytes
+  .byte %00000000, %00000000, %00000000, %00000000, %00000000, %00000000, %00000000, %00000000
+  .byte %00000000, %00000000, %00000000, %00000000, %00000000, %00000000, %00000000, %00000000
+  .byte %00000000, %00000000, %00000000, %00000000, %00000000, %00000000, %00000000, %00000000
+  .byte %00000000, %00000000, %00000000, %00000000, %00000000, %00000000, %00000000, %00000000
+  .byte %00000000, %00000000, %00000000, %00000000, %00000000, %00000000, %00000000, %00000000
+  .byte %00000000, %00000000, %00000000, %00000000, %00000000, %00000000, %00000000, %00000000
+  .byte %00000000, %00000000, %00000000, %00000000, %00000000, %00000000, %00000000, %00000000
+  .byte %00000000, %00000000, %00000000, %00000000, %00000000, %00000000, %00000000, %00000000
 
-nametabledata:
-    .byte $24,$24,$24,$24,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00   ; row1
-    .byte $00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$24,$24,$24,$24
+background:
 
-    .byte $24,$24,$24,$24,$00,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24   ; row 2
-    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$00,$24,$24,$24,$24
+    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24
+    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24
 
-    .byte $24,$24,$24,$24,$00,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24   ; row 2
-    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$00,$24,$24,$24,$24
-
-    .byte $24,$24,$24,$24,$00,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24   ; row 2
-    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$00,$24,$24,$24,$24
-
-    .byte $24,$24,$24,$24,$00,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24   ; row 2
-    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$00,$24,$24,$24,$24
-
-    .byte $24,$24,$24,$24,$00,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24   ; row 2
-    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$00,$24,$24,$24,$24
-
-    .byte $24,$24,$24,$24,$00,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24   ; row 2
-    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$00,$24,$24,$24,$24
-
-    .byte $24,$24,$24,$24,$00,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24   ; row 2
-    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$00,$24,$24,$24,$24
-
-    .byte $24,$24,$24,$24,$00,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24   ; row 2
-    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$00,$24,$24,$24,$24
-
-    .byte $24,$24,$24,$24,$00,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24   ; row 2
-    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$00,$24,$24,$24,$24
-
-    .byte $24,$24,$24,$24,$00,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24   ; row 2
-    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$00,$24,$24,$24,$24
-
-    .byte $24,$24,$24,$24,$00,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24   ; row 2
-    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$00,$24,$24,$24,$24
-
-    .byte $24,$24,$24,$24,$00,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24   ; row 2
-    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$00,$24,$24,$24,$24
-
-    .byte $24,$24,$24,$24,$00,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24   ; row 2
-    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$00,$24,$24,$24,$24
+    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24
+    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24
 
     .byte $24,$24,$24,$24,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00   ; row1
     .byte $00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$24,$24,$24,$24
+
+    .byte $24,$24,$24,$24,$00,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24   ; row 2
+    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$00,$24,$24,$24,$24
+
+    .byte $24,$24,$24,$24,$00,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24   ; row 3
+    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$00,$24,$24,$24,$24
+
+    .byte $24,$24,$24,$24,$00,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24   ; row 4
+    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$00,$24,$24,$24,$24
+
+    .byte $24,$24,$24,$24,$00,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24   ; row 5
+    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$00,$24,$24,$24,$24
+
+    .byte $24,$24,$24,$24,$00,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24   ; row 6
+    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$00,$24,$24,$24,$24
+
+    .byte $24,$24,$24,$24,$00,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24   ; row 7
+    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$00,$24,$24,$24,$24
+
+    .byte $24,$24,$24,$24,$00,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24   ; row 7
+    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$00,$24,$24,$24,$24
+
+    .byte $24,$24,$24,$24,$00,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24   ; row 8
+    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$00,$24,$24,$24,$24
+
+    .byte $24,$24,$24,$24,$00,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24   ; row 9
+    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$00,$24,$24,$24,$24
+
+    .byte $24,$24,$24,$24,$00,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24   ; row 10
+    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$00,$24,$24,$24,$24
+
+    .byte $24,$24,$24,$24,$00,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24   ; row 11
+    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$00,$24,$24,$24,$24
+
+    .byte $24,$24,$24,$24,$00,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24   ; row 12
+    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$00,$24,$24,$24,$24
+
+    .byte $24,$24,$24,$24,$00,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24   ; row 13
+    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$00,$24,$24,$24,$24
+
+    .byte $24,$24,$24,$24,$00,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24   ; row 14
+    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$00,$24,$24,$24,$24
+
+    .byte $24,$24,$24,$24,$00,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24   ; row 15
+    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$00,$24,$24,$24,$24
+
+    .byte $24,$24,$24,$24,$00,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24   ; row 16
+    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$00,$24,$24,$24,$24
+
+    .byte $24,$24,$24,$24,$00,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24   ; row 17
+    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$00,$24,$24,$24,$24
+
+    .byte $24,$24,$24,$24,$00,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24   ; row 18
+    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$00,$24,$24,$24,$24
+
+    .byte $24,$24,$24,$24,$00,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24   ; row 19
+    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$00,$24,$24,$24,$24
+
+    .byte $24,$24,$24,$24,$00,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24   ; row 20
+    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$00,$24,$24,$24,$24
+
+    .byte $24,$24,$24,$24,$00,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24   ; row 21
+    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$00,$24,$24,$24,$24
+
+    .byte $24,$24,$24,$24,$00,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24   ; row 22
+    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$00,$24,$24,$24,$24
+
+    .byte $24,$24,$24,$24,$00,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24   ; row 23
+    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$00,$24,$24,$24,$24
+
+    .byte $24,$24,$24,$24,$00,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24   ; row 26
+    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$00,$24,$24,$24,$24
+
+    .byte $24,$24,$24,$24,$00,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24   ; row 27
+    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$00,$24,$24,$24,$24
+
+    .byte $24,$24,$24,$24,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00   ; row 30
+    .byte $00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$24,$24,$24,$24
+
+    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24
+    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24
+
+    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24
+    .byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24
+
+sprites:
+    .byte $00, $00, $00, $08 ; YCoord, tile number, attr, XCoord
+    .byte $00, $01, $00, $10
+    .byte $08, $02, $00, $08
+    .byte $08, $03, $00, $10
+    .byte $10, $04, $00, $08
+    .byte $10, $05, $00, $10
+    .byte $18, $06, $00, $08
+    .byte $18, $07, $00, $10
 
 
 .segment "VECTORS"
